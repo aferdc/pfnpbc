1c808917b9a4f69866274bd5fe3d6499bf4dab63
aferdc <acroce@gmail.com>
2013-10-06 10:14:47 -0700

    First Commit
    This is the code from day 2 of PFNP Bootcamp.

---
+++ jquery-game/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$("#main").click(changeColors);
+});
+
+var colors = ["orange", "red", "green", "blue", "pink", "yellow"]
+
+var changeColors = function(){
+	var randomNum1 = Math.floor(Math.random() * colors.length);
+	var randomNum2 = Math.floor(Math.random() * colors.length);
+
+	var randomColor1 = colors[randomNum1];
+	var randomColor2 = colors[randomNum2];
+
+	$("h2").text(randomColor1);
+	$("h2").css("color", randomColor2);
+
+
+}
